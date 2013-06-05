0b922969a13eaea9138ad09d42197caf71b6a530
Danny Sun <sunnyhust2005@gmail.com>
2013-06-05 13:33:26 +0800

    shows the A club all the time

--- Walkthrough/Matchismo/Matchismo/CardGameViewController.m
+++ Walkthrough/Matchismo/Matchismo/CardGameViewController.m
@@ -9,21 +9,22 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
 @end
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (void)setFlipCount:(int)flipCount {
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)flipCard:(UIButton *)sender {
+    sender.selected = !sender.isSelected;
+    self.flipCount++;
 }
+
+
 
 @end
