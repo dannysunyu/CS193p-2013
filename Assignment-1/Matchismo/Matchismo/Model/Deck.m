2811f6eb30ae33010fd0292e13d73c20b87963de
Danny Sun <sunnyhust2005@gmail.com>
2013-06-06 14:04:49 +0800

    add starter files for Assignment-1

---
+++ Assignment-1/Matchismo/Matchismo/Model/Deck.m
@@ -0,0 +1,42 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by 孙 昱 on 13-6-5.
+//  Copyright (c) 2013年 CS193p. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface Deck()
+@property (strong, nonatomic) NSMutableArray *cards;
+@end
+
+@implementation Deck
+
+- (NSMutableArray *)cards {
+    if (!_cards) _cards = [[NSMutableArray alloc] init];
+    return _cards;
+}
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop {
+    if (atTop) {
+        [self.cards insertObject:card atIndex:0];
+    } else {
+        [self.cards addObject:card];
+    }
+}
+
+- (Card *)drawRandomCard {
+    Card *randomCard = nil;
+    
+    if (self.cards.count) {
+        unsigned index = arc4random() % self.cards.count;
+        randomCard = self.cards[index];
+        [self.cards removeObjectAtIndex:index];
+    }
+    
+    return randomCard;
+}
+
+@end
