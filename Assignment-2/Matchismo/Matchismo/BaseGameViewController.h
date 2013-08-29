9bed4c5e6680ba5df85ec80749c44506bcdc325b
Danny Sun <sunnyhust2005@gmail.com>
2013-08-29 22:59:20 +0800

    Subclassing to share functionality

---
+++ Assignment-2/Matchismo/Matchismo/BaseGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseGameViewController.h
+//  Matchismo
+//
+//  Created by 孙 昱 on 13-8-29.
+//  Copyright (c) 2013年 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseGameViewController : UIViewController
+
+@end
---
+++ Assignment-2/Matchismo/Matchismo/BaseGameViewController.m
@@ -0,0 +1,38 @@
+//
+//  BaseGameViewController.m
+//  Matchismo
+//
+//  Created by 孙 昱 on 13-8-29.
+//  Copyright (c) 2013年 CS193p. All rights reserved.
+//
+
+#import "BaseGameViewController.h"
+
+@interface BaseGameViewController ()
+
+@end
+
+@implementation BaseGameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
