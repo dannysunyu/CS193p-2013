3fceb9047fd5c27bdc495cb5b704b4957a56aeda
Danny Sun <sunnyhust2005@gmail.com>
2013-09-07 21:44:04 +0800

    refresh

--- Pretty Matchismo/Matchismo/CardGameViewController.h
+++ Pretty Matchismo/Matchismo/CardGameViewController.h
@@ -14,8 +14,11 @@
 @interface CardGameViewController : UIViewController
 
 // all of the following methods must be overriden by concrete subclasses
+
 - (Deck *)createDeck; // abstract
+
 @property (readonly, nonatomic) NSUInteger startingCardCount; // abstract
+
 - (void)updateCell:(UICollectionViewCell *)cell usingCard:(Card *)card; // abstract
 
 @end
