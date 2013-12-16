753a4fa6e12dc942f1f3c6b027aacafc0df45a0e
Danny Sun <sunnyhust2005@gmail.com>
2013-12-16 22:48:56 +0800

    add a template for Assigment 6

---
+++ Assignment-6/CoreDataSPoT/CoreDataSPoT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreDataSPoT
+//
+//  Created by 孙 昱 on 13-12-16.
+//  Copyright (c) 2013年 CS193p. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
